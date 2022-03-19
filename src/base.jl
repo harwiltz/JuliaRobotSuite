e1d6ef42abea67250ad1a04f355c4e0b5142010a
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2022-03-19 12:42:41 -0400

    Adds lazy visualizer for rendering meshcat

--- src/base.jl
+++ src/base.jl
@@ -1,3 +1,27 @@
-export AbstractRobotEnv
+export AbstractRobotEnv, LazyValue, BaseMeshCatVisualizer, acquire!
+
+using MeshCat
 
 abstract type AbstractRobotEnv <: AbstractEnv end
+
+mutable struct LazyValue{V}
+    init::Any
+    val::Union{Missing, V}
+end
+
+LazyValue{V}(init) where {V} = LazyValue{V}(init, missing)
+
+function acquire!(lv::LazyValue)
+    if ismissing(lv.val)
+        setfield!(lv, :val, lv.init())
+    end
+    lv.val
+end
+
+function BaseMeshCatVisualizer(window = true)
+    vis = Visualizer()
+    if window
+        open(vis)
+    end
+    vis
+end
--- src/pendulums/double_pendulum.jl
+++ src/pendulums/double_pendulum.jl
@@ -5,58 +5,29 @@ using ..JuliaRobotSuite
 using LinearAlgebra
 using StaticArrays
 
-import RigidBodyDynamics as RBD
+using MeshCat
+using MeshCatMechanisms
+using RigidBodyDynamics
 
-const G = -9.81
+import Plots
+
+const PENDULUM_URDF = "models/double-pendulum.urdf"
 
 mutable struct DoublePendulumEnv <: AbstractRobotEnv
-    pendulum::RBD.Mechanism
+    pendulum::Mechanism
+    vis::LazyValue{Visualizer}
 end
 
-function DoublePendulumEnv(
-    ;
-    F = Float64,
-    str_world = "world",
-    I₁ = 0.333,
-    c₁ = 0.5,
-    m₁ = 1.,
-    l₁ = 1.,
-    I₂ = 0.333,
-    c₂ = 0.5,
-    m₂ = 1.,
-)
-    world = RBD.RigidBody{F}(str_world)
-    pendulum = RBD.Mechanism(world; gravity = SVector(0, 0, G))
-    axis = SVector(0., 1., 0.)
-    frame₁ = RBD.CartesianFrame3D("upperlink")
-    inertia₁ = RBD.SpatialInertia(frame₁,
-                                  moment = I₁ * axis * axis',
-                                  com = SVector(0, 0, -c₁),
-                                  mass = m₁)
-    upperlink = RBD.RigidBody(inertia₁)
-    shoulder = RBD.Joint("shoulder", RBD.Revolute(axis))
-    before_shoulder_to_world = one(RBD.Transform3D, RBD.frame_before(shoulder), RBD.default_frame(world))
-    RBD.attach!(pendulum, world, upperlink, shoulder, joint_pose = before_shoulder_to_world)
+DoublePendulumEnv() = DoublePendulumEnv(parse_urdf(PENDULUM_URDF),
+                                        LazyValue{Visualizer}(BaseMeshCatVisualizer))
 
-    frame₂ = RBD.CartesianFrame3D("lowerlink")
-    inertia₂ = RBD.SpatialInertia(frame₂,
-                                  moment = I₂ * axis * axis',
-                                  com = SVector(0, 0, -c₂),
-                                  mass = m₂)
-    lowerlink = RBD.RigidBody(inertia₂)
-    elbow = RBD.Joint("elbow", RBD.Revolute(axis))
-    before_elbow_to_after_shoulder = RBD.Transform3D(RBD.frame_before(elbow),
-                                                     RBD.frame_after(shoulder),
-                                                     SVector(0, 0, -l₁))
-    RBD.attach!(pendulum, upperlink, lowerlink, elbow, joint_pose = before_elbow_to_after_shoulder)
+function Plots.plot(env::DoublePendulumEnv)
+    vis = acquire!(env.vis)
+    mvis = MechanismVisualizer(env.pendulum, URDFVisuals(PENDULUM_URDF), vis)
+end
 
-    state = RBD.MechanismState(pendulum)
-    RBD.set_configuration!(state, shoulder, 0.3)
-    RBD.set_configuration!(state, elbow, 0.4)
-    RBD.set_velocity!(state, shoulder, 1.)
-    RBD.set_velocity!(state, elbow, 2.)
-
-    DoublePendulumEnv(pendulum)
+function Base.show(io::IOContext{Base.TTY}, t::MIME{Symbol("text/plain")}, env::DoublePendulumEnv)
+    Base.show(env.pendulum)
 end
 
 end
